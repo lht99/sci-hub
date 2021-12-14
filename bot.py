719888f2f7bf0eb6a146446914b59a6d049c5b40
lht99 <33472127+lht99@users.noreply.github.com>
2021-12-14 15:39:07 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,37 +25,42 @@ persistence = PicklePersistence('./db', store_user_data = True)
 def start(update, context):
     update.message.reply_text('Xin chào, mình lập Bot này để hỗ trợ mọi người tải file pdf từ sci-hub')
 def sci(update, context):
-    ids = update.message.message_id
-    chat_id =  update.message.chat_id
-    ur = update.message.text
-    sci_url = 'https://sci-hub.se/' + str(ur)
-    html_text = requests.get(sci_url).text
-    soup = bs(html_text, 'html.parser')
-    link = soup.findAll("button")
-    title = soup.findAll('i')
-    link1 = link[0]
-    link2 = link1
-    link3 = link2["onclick"]
-    link4 = link3.split("'")
-    link5 = link4[1]
-    if link5[:2] == "//":
-        link6 = link5.replace("//", "http://")
-        update.message.reply_text(link6)
-    else:
-        link6 = link5
-        update.message.reply_text(link6)
-    
-    title1 = title[0].text.split(".")[0]
-    if len(title1) == 0:
-        title2 = "your file.pdf"
-    else:
-        title2 = title1 + ".pdf"
-    response = requests.get(link6)
-    with open(title2, 'wb') as f:
-                              f.write(response.content)
-    f.close()
-    update.message.reply_text("Your output file: \n")
-    context.bot.send_document(chat_id, open(title2, 'rb'),  reply_to_message_id = ids)                        
+    try:
+        ids = update.message.message_id
+        chat_id = update.message.chat_id
+        ur = update.message.text
+        sci_url = 'https://sci-hub.se/' + str(ur)
+        html_text = requests.get(sci_url).text
+        soup = bs(html_text, 'html.parser')
+        link = soup.findAll("button")
+        title = soup.findAll('i')
+        link1 = link[0]
+        link2 = link1
+        link3 = link2["onclick"]
+        link4 = link3.split("'")
+        link5 = link4[1]
+        if link5[:2] == "//":
+            link6 = link5.replace("//", "http://")
+            update.message.reply_text(link6)
+        else:
+            link6 = link5
+            update.message.reply_text(link6)
+
+        title1 = title[0].text.split(".")[0]
+        if len(title1) == 0:
+            title2 = "your file.pdf"
+        else:
+            title2 = title1 + ".pdf"
+        response = requests.get(link6)
+        with open(title2, 'wb') as f:
+            f.write(response.content)
+        f.close()
+        update.message.reply_text("Your output file: \n")
+        context.bot.send_document(chat_id, open(
+            title2, 'rb'),  reply_to_message_id=ids)
+    except:
+        update.message.reply_text("File not found")
+        
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
