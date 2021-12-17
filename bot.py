d1fc614d2917aa7ff18b80cd143a8c887ad15976
lht99 <33472127+lht99@users.noreply.github.com>
2021-12-17 17:18:52 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ def start(update, context):
 def sci(update, context):
     ids = update.message.message_id
     chat_id = update.message.chat_id
-    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.57'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'}
     ur = update.message.text
     sci_url = 'https://sci-hub.se/' + str(ur)
     html_text = requests.get(sci_url, headers=headers).text
