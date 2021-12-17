7fed4376b43eba8bc71cb87b9ec7c2d2076a2cff
lht99 <33472127+lht99@users.noreply.github.com>
2021-12-17 21:56:34 +0900

    Update bot.py
    Hic

--- bot.py
+++ bot.py
@@ -39,7 +39,8 @@ def sci(update, context):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.57', 'referer': 'https://sci-hub.se/', 'upgrade-insecure-requests': '1'}
     ur = update.message.text
     sci_url = 'https://sci-hub.se/' + str(ur)
-    html_text = requests.get(sci_url, headers = headers).text
+    session = requests.Session()
+    html_text = session.get(sci_url, headers = headers).text
     soup = bs(html_text, 'html.parser')
     link = soup.findAll("button")
     title = soup.findAll('i')
