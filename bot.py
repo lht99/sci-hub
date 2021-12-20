a85cf7c4822063e278f09e2738e13040ea0ce1ac
lht99 <33472127+lht99@users.noreply.github.com>
2021-12-20 22:33:50 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ def sci(update, context):
     #headers = {
         #'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.57'}
     ur = update.message.text
-    sci_url = 'https://sci-hub.se/' + str(ur)
+    sci_url = 'http://sci-hub.se/' + str(ur)
     html = requests.Session()
     html_t = html.post(sci_url, headers = headers)
     html_text = html_t.text
