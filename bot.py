b50454366b26caf02284d75d2c4c0adf3d03a78d
lht99 <33472127+lht99@users.noreply.github.com>
2021-12-20 18:17:41 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,13 @@ def start(update, context):
 def sci(update, context):
     ids = update.message.message_id
     chat_id = update.message.chat_id
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.57'
+    headers = {'cache-control': 'max-age=0',
+               'referer': 'https://sci-hub.se/',
+               'sec-fetch-mode': 'navigate',
+               'sec-fetch-site': 'same-origin',
+               'sec-fetch-user': '?1',
+               'upgrade-insecure-requests': '1',
+               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.57'
     }
 
     #headers = {
