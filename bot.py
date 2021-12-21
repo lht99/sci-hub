1541e3e80971da3491b21f6f1e25640af9b344f5
lht99 <33472127+lht99@users.noreply.github.com>
2021-12-21 23:14:46 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,9 +53,9 @@ def sci(update, context):
     #headers = {
         #'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.57'}
     ur = update.message.text
-    sci_url = 'http://sci-hub.se/' + str(ur)
+    sci_url = 'https://sci-hub.se/' + str(ur)
     html = requests.Session()
-    html_t = html.get(sci_url, headers = headers, verify=False)
+    html_t = html.get(sci_url, headers = headers)
     html_text = html_t.text
     soup = bs(html_text, 'html.parser')
     link = soup.findAll("button")
