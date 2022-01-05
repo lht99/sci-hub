46440eaee27a290bf5c92bdbaacd05eaccd16f60
lht99 <33472127+lht99@users.noreply.github.com>
2022-01-05 14:29:09 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ def sci(update, context):
                'cache-control': 'no-cache',
                'content-length': '110',
                'content-type': 'application/x-www-form-urlencoded',
-               'origin': 'https://sci-hub.se'
+               'origin': 'https://sci-hub.se',
                'referer': u,
                'pragma': 'no-cache',
                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62'
