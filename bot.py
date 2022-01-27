a9d3e2c749c63434d87b50bb2eaebbf6512824a8
lht99 <33472127+lht99@users.noreply.github.com>
2022-01-27 13:37:18 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -120,7 +120,7 @@ def main():
     dp.add_error_handler(error)
     updater.start_webhook(listen="0.0.0.0", port=int(
         PORT), url_path=TOKEN, webhook_url="https://sci-letuan.herokuapp.com/" + TOKEN)
-    updater.idle()
+    #updater.idle()
 
 
 if __name__ == '__main__':
